457221935aa79c3ef024368d00a22771c91792d1
Krushant <krushantw@gmail.com>
2025-06-07 22:00:06 +0530

    Update next.config.mjs to enhance image loading and security headers

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,23 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
